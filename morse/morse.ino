a4bf7d502dec00c8f48b9076a6cda081178e8cb9
徐荣军 <52456117+shendasanguo@users.noreply.github.com>
2019-07-05 10:42:25 +0800

    Update morse.ino

--- morse/morse.ino
+++ morse/morse.ino
@@ -1,16 +1,237 @@
-#include <Morse.h>
-Morse m(13);
+#include <Stepper.h>
+
+
 char income;
-int i=0;
-void setup() {
-  // put your setup code here, to run once:
+int i,n=0;
+
+void setup()
+{
+  pinMode(2, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()>0){
-    income=Serial.read();
-    m.changeMorse(Code[i],m);
-    }
+void loop()
+{
+  if(Serial.available()>0)
+  {
+       income=Serial.read();
+       changeMorse(income);
+  }
+}
+void dot()
+{
+	digitalWrite(2,HIGH);
+	delay(250);
+	digitalWrite(2,LOW);
+	delay(250);
+}
+
+void dash()
+{
+	digitalWrite(2,HIGH);
+	delay(1000);
+	digitalWrite(2,LOW);
+	delay(250);
+}
+void changeMorse(char ch)
+{
+	switch(ch){
+		case'a':{
+			dot();
+			dash();
+            c_space();
+			break;
+		}
+		case'b':{
+			dash();
+			dot();
+			dot();
+			dot();
+            c_space();
+			break;
+		}
+		case'c':{
+			dash();
+			dot();
+			dash();
+			dot();
+            c_space();
+			break;
+		}
+		case'd':{
+			dash();
+		    dot();
+			dot();
+            c_space();
+			break;
+		}
+		case'e':{
+			dot();
+            c_space();
+			break;
+		}
+		case'f':{			
+			dot();
+			dot();
+			dash();
+			dot();
+            c_space();
+			break;
+		}
+		case'g':{			
+			dash();
+			dash();
+			dot();
+            c_space();
+			break;
+		}
+		case'h':{			
+            dot();
+			dot();
+			dot();
+			dot();
+            c_space();
+			break;
+		}
+		case'i':{			
+			dot();
+		    dot();
+            c_space();
+			break;
+		}
+		case'j':{			
+			dot();
+			dash();
+			dash(); 
+			dash(); 
+            c_space();              
+			break;
+		}
+		case'k':{						
+			dash();
+			dot(); 
+			dash();
+            c_space();
+			break;
+		}
+		case'l':{						
+			dot(); 
+			dash();
+			dot();
+			dot(); 
+            c_space();
+			break;
+		}
+		case'm':{						 
+			dash();
+			dash(); 
+            c_space();
+			break;
+		}
+		case'n':{						 
+			dash();
+			dot(); 
+            c_space();
+			break;
+		}
+		case'o':{						 
+			dash();
+			dash();
+			dash(); 
+            c_space();
+			break;
+		}
+		case'p':{						 
+			dot();
+			dash();
+			dash();
+			dot(); 
+            c_space();
+			break;
+		}
+		case'q':{						 
+			dash();
+			dash();
+			dot();
+			dash(); 
+            c_space();
+			break;
+		}
+		case'r':{						 
+			dot();
+			dash();
+			dot();
+            c_space();
+			break;
+		}
+		case's':{						 
+			dot();
+			dot();
+			dot();
+            c_space();
+			break;
+		}
+		case't':{						 
+			dash();
+            c_space();
+			break;
+		}
+		case'u':{
+		    dot();
+			dot();						 
+			dash();
+            c_space();
+			break;
+		}
+		case'v':{
+		    dot();
+			dot();
+			dot();						 
+			dash();
+            c_space();
+			break;
+		}
+		case'w':{
+		    dot();						 
+			dash();
+			dash();
+            c_space();
+			break;
+		}
+		case'x':{		    						 
+			dash();
+			dot();
+			dot();
+			dash();
+            c_space();
+			break;
+		}
+		case'y':{		    						 
+			dash();
+			dot();
+			dash();
+			dash();
+            c_space();
+			break;
+		}
+		case'z':{		    						 
+			dash();
+			dash();
+			dot();
+			dot();
+            c_space();
+			break;
+		}
+		default: w_space();
+	}
+}
+void w_space()
+{
+  	digitalWrite(2,LOW);
+	delay(1750);
+}
+void c_space()
+{
+  digitalWrite(2,LOW);
+  delay(750);
 }
