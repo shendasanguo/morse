fe91a3897e0123f320aff120e921e2c90d4145d8
徐荣军 <52456117+shendasanguo@users.noreply.github.com>
2019-07-05 09:23:21 +0800

    Update Morse.cpp

--- morse/Morse.cpp
+++ morse/Morse.cpp
@@ -22,125 +22,147 @@ void Morse::dash()
 	digitalWrite(_pin,LOW);
 	delay(250);
 }
-void Morse::changeMorse(char ch,Morse m){
-	switch(ch){
-		case'a':{
+void Morse::changeMorse(char ch,Morse m)
+{
+	switch(ch)
+	{
+		case'a':
+		{
 			m.dot();
 			m.dash();
 			break;
 		}
-		case'b':{
+		case'b':
+		{
 			m.dash();
 			m.dot();
 			m.dot();
 			m.dot();
 			break;
 		}
-		case'c':{
+		case'c':
+		{
 			m.dash();
-			m.dot();
-			m.dash();
-			m.dot();
-			break;
-		}
-		case'd':{
-			m.dash();
-			m.dot();
-			m.dot();
-			break;
-		}
-		case'e':{
-			m.dot();
-			break;
-		}
-		case'f':{			
-			m.dot();
 			m.dot();
 			m.dash();
 			m.dot();
 			break;
 		}
-		case'g':{			
+		case'd':
+		{
+			m.dash();
+			m.dot();
+			m.dot();
+			break;
+		}
+		case'e':
+		{
+			m.dot();
+			break;
+		}
+		case'f':
+		{			
+			m.dot();
+			m.dot();
+			m.dash();
+			m.dot();
+			break;
+		}
+		case'g':
+		{			
 			m.dash();
 			m.dash();
 			m.dot();
 			break;
 		}
-		case'h':{			
-            m.dot();
+		case'h':
+		{			
+                        m.dot();
 			m.dot();
 			m.dot();
 			m.dot();
 			break;
 		}
-		case'i':{			
+		case'i':
+		{			
 			m.dot();
 			m.dot();
 			break;
 		}
-		case'j':{			
+		case'j':
+		{			
 			m.dot();
 			m.dash();
 			m.dash(); 
 			m.dash(); 
 			break;
 		}
-		case'k':{						
+		case'k':
+		{						
 			m.dash();
 			m.dot(); 
 			m.dash(); 
 			break;
 		}
-		case'l':{						
+		case'l':
+		{						
 			m.dot(); 
 			m.dash();
 			m.dot();
 			m.dot(); 
 			break;
 		}
-		case'm':{						 
+		case'm':
+		{						 
 			m.dash();
 			m.dash(); 
 			break;
 		}
-		case'n':{						 
+		case'n':
+		{						 
 			m.dash();
 			m.dot(); 
 			break;
 		}
-		case'o':{						 
+		case'o':
+		{						 
 			m.dash();
 			m.dash();
 			m.dash(); 
 			break;
 		}
-		case'p':{						 
+		case'p':
+		{						 
 			m.dot();
 			m.dash();
 			m.dash();
 			m.dot(); 
 			break;
 		}
-		case'q':{						 
+		case'q':
+		{						 
 			m.dash();
 			m.dash();
 			m.dot();
 			m.dash(); 
 			break;
 		}
-		case'r':{						 
+		case'r':
+		{						 
 			m.dot();
 			m.dash();
 			m.dot();
 			break;
 		}
-		case's':{						 
+		case's':
+		{						 
 			m.dot();
 			m.dot();
 			m.dot();
 			break;
 		}
-		case't':{						 
+		case't':
+		{						 
 			m.dash();
 			break;
 		}
@@ -150,34 +172,39 @@ void Morse::changeMorse(char ch,Morse m){
 			m.dash();
 			break;
 		}
-		case'v':{
+		case'v':
+		{
 		    m.dot();
 			m.dot();
 			m.dot();						 
 			m.dash();
 			break;
 		}
-		case'w':{
+		case'w':
+		{
 		    m.dot();						 
 			m.dash();
 			m.dash();
 			break;
 		}
-		case'x':{		    						 
+		case'x':
+		{		    						 
 			m.dash();
 			m.dot();
 			m.dot();
 			m.dash();
 			break;
 		}
-		case'y':{		    						 
+		case'y':
+		{		    						 
 			m.dash();
 			m.dot();
 			m.dash();
 			m.dash();
 			break;
 		}
-		case'z':{		    						 
+		case'z':
+		{		    						 
 			m.dash();
 			m.dash();
 			m.dot();
